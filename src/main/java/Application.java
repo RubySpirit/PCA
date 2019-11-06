da443b7d0228742bb7b6b60acd85e67dabf8e028
SpiritusPoland <lkszmalinowski@gmail.com>
2019-11-06 23:06:05 +0100

    changes in .gitignore small refactor

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,6 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.math3.linear.EigenDecomposition;
@@ -17,22 +18,27 @@ public class Application {
     private static BufferedWriter bw;
 
     public static void main(String[] args) throws IOException {
-        //loading file:
+        //załadowanie pliku do pamięci:
         List<BankCsvModel> modelList = CsvReader.getBankModelList("bank.csv");
         double[][] array2d = new double[modelList.size()][];
         for (int i = 0; i < modelList.size(); i++) {
             array2d[i] = modelList.get(i).toDoubleArray();
         }
-
+        //zamiana tablicy 2d na macierz
         RealMatrix realmatrix = MatrixUtils.createRealMatrix(array2d);
+        //obliczenie kowariancji z macierzy
         Covariance covariance = new Covariance(realmatrix);
+        //zapisanie kowariancji do macierzy
         RealMatrix covarianceMatrix = covariance.getCovarianceMatrix();
+        //utworzenie obiektu odpowiedzialnego za dekompozycje na wartości własne, wektory własne
         EigenDecomposition ed = new EigenDecomposition(covarianceMatrix);
         double[][] covarianceArray = covarianceMatrix.getData();
-        double[] eigenValues = ed.getRealEigenvalues();
+        //wyznaczenie wartosci własnych
+        double [] eigenValues = ed.getRealEigenvalues();
+        //utworzenie macierzy W
         double[][] macierzW = ed.getV().getData();
-
         RealMatrix Wmatrix = ed.getV();
+        //pomnożenie macierzy W przez DataSet
         RealMatrix wynik = realmatrix.multiply(Wmatrix);
 
 
@@ -40,14 +46,15 @@ public class Application {
         bw = new BufferedWriter(new FileWriter(covariancefile));
         saveWithNewLine("Macierz kowariancji:");
         printMatrix(covarianceArray, 2, bw);
+        Arrays.stream(eigenValues).forEach(value -> saveWithNewLine("Wartosc wlasna:;" + value));
         for (int i = 0; i < eigenValues.length; i++) {
-            saveWithNewLine("Wartosc wlasna:;" + ed.getRealEigenvalue(i) + " ");
-            saveWithNewLine("Wektor wlasny:;");
             for (double var : ed.getEigenvector(i).toArray()) {
+                saveWithNewLine("Wektor wlasny:;");
                 bw.write(var + ";");
             }
             bw.newLine();
         }
+
         saveWithNewLine("Macierz W");
         printMatrix(macierzW, 7, bw);
         saveWithNewLine("Wynik pomnozenia bazy przez Macierz W:");
@@ -66,8 +73,14 @@ public class Application {
         bw.newLine();
     }
 
-    private static void saveWithNewLine(String a) throws IOException {
-        bw.write(a);
-        bw.newLine();
+    private static void saveWithNewLine(String a) {
+        try {
+            bw.write(a);
+            bw.newLine();
+        }
+        catch (IOException ioe)
+        {
+            System.out.println("problem z plikiem");
+        }
     }
 }
